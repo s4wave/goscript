b5bffd6a22129ce89f97c13029cc505a52027064
Christian Stewart <christian@aperture.us>
2026-07-16 21:14:58 -0700

    fix(syscall): add sigkill constant
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- tests/tests/package_import_syscall/package_import_syscall.gs.ts
+++ tests/tests/package_import_syscall/package_import_syscall.gs.ts
@@ -33,6 +33,7 @@ export async function main(): globalThis.Promise<void> {
 	if ((syscall.F_DUPFD_CLOEXEC as number) != 0) {
 		fmt.Println("cloexec supported")
 	}
+	fmt.Println("signals:", $.namedValueInterfaceValue<any>($.int(syscall.SIGINT), "int", {}, { kind: $.TypeKind.Basic, name: "int" }), $.namedValueInterfaceValue<any>($.int(syscall.SIGKILL), "int", {}, { kind: $.TypeKind.Basic, name: "int" }), $.namedValueInterfaceValue<any>($.int(syscall.SIGTERM), "int", {}, { kind: $.TypeKind.Basic, name: "int" }))
 	if (false) {
 		let st: $.VarRef<syscall.Stat_t> = $.varRef($.markAsStructValue(new syscall.Stat_t()))
 		let buf: $.Slice<number> = null as $.Slice<number>
